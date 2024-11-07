f7ec793fbba71b61453f92e1ca9395e779d6b1a0
VIE <anq.viet203@gmail.com>
2024-11-07 16:36:13 +0700

    Update send email to candidate

---
+++ lab-week-05/lab_week05_AnQuocViet_21080821/src/main/java/fit/se/backend/configs/WebConfig.java
@@ -0,0 +1,15 @@
+package fit.se.backend.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+/**
+ * @description
+ * @author: vie
+ * @date: 7/11/24
+ */
+@Configuration
+public class WebConfig {
+
+}
--- lab-week-05/lab_week05_AnQuocViet_21080821/src/main/java/fit/se/backend/utils/SimpleJobManager.java
+++ lab-week-05/lab_week05_AnQuocViet_21080821/src/main/java/fit/se/backend/utils/SimpleJobManager.java
@@ -2,11 +2,13 @@ package fit.se.backend.utils;
 
 import fit.se.backend.dtos.CandidateDto;
 import fit.se.backend.dtos.JobDto;
-import lombok.Setter;
+import jakarta.mail.Message;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Level;
@@ -17,35 +19,50 @@ import java.util.logging.Logger;
  * @author: vie
  * @date: 7/11/24
  */
-@Setter
 @Service
 public class SimpleJobManager implements JobManager {
-   private MailSender mailSender;
-   private SimpleMailMessage templateMessage;
+   private JavaMailSender mailSender;
    private final Logger logger = Logger.getLogger(SimpleJobManager.class.getName());
 
+   @Autowired
+   public void setMailSender(JavaMailSender mailSender) {
+      this.mailSender = mailSender;
+   }
+
    public void sendEmail(JobDto job, CandidateDto candidateDto) {
+      MimeMessagePreparator preparator = mimeMessage -> {
+         mimeMessage.setRecipient(Message.RecipientType.TO,
+               new InternetAddress(candidateDto.email())
+         );
+         mimeMessage.setSubject("Job Invitation");
+         mimeMessage.setFrom(new InternetAddress(job.companyEmail()));
+         String emailBody = """
+               Dear %s,
+               
+               We are pleased to invite you to apply for the position of %s at our company.
+               
+               Job Description:
+               %s
+               
+               Please let us know if you are interested.
+               
+               Best regards,
+               %s
+               """.formatted(
+               candidateDto.fullName(),
+               job.jobName(),
+               job.jobDesc(),
+               job.companyName()
+         );
 
-      // Do the business calculations...
+         mimeMessage.setText(emailBody);
 
-      // Call the collaborators to persist the order...
-
-      // Create a thread-safe "copy" of the template message and customize it
-      SimpleMailMessage msg = new SimpleMailMessage(this.templateMessage);
-      msg.setFrom(job.companyEmail());
-      msg.setTo(candidateDto.email());
-      msg.setText("Dear " + candidateDto.fullName() + ",\n\n" +
-                        "We are pleased to invite you to apply for the position of " + job.jobName() + " at our company.\n\n" +
-                        "Job Description:\n" + job.jobDesc() + "\n\n" +
-                        "Please let us know if you are interested.\n\n" +
-                        "Best regards,\n" +
-                        job.companyName());
+      };
       try {
-         this.mailSender.send(msg);
+         this.mailSender.send(preparator);
       } catch (MailException ex) {
          logger.log(Level.SEVERE, "An error occurred while sending the email: " + ex.getMessage());
          throw new RuntimeException("An error occurred while sending the email: " + ex.getMessage());
       }
    }
-
 }
